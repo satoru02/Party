d868a8d6f81603512e5a32073524203e662db8c3
satoru02 <sugiwaka02@gmail.com>
2021-01-10 10:27:28 +0900

    user_mordal

--- app/javascript/components/perpage/TheHeader.vue
+++ app/javascript/components/perpage/TheHeader.vue
@@ -20,9 +20,22 @@
           <v-icon size=23>mdi-chat</v-icon>
         </v-col>
         <v-col>
-          <router-link :to="{ name: 'User', params: {id: `${this.$store.state.currentUser.id}`} }">
-            <avatar class="mt-9 mr-n8"></avatar>
-          </router-link>
+          <!-- <router-link :to="{ name: 'User', params: {id: `${this.$store.state.currentUser.id}`} }"> -->
+          <!-- <avatar class="mt-9 mr-n8"></avatar> -->
+          <!-- <userMenu></userMenu> -->
+          <!-- </router-link> -->
+          <v-menu open-on-hover offset-y left nudge-height=800 nudge-width=150 >
+            <template v-slot:activator="{ on, attrs }">
+              <div v-bind="attrs" v-on="on">
+                <avatar class="mt-9 mr-n8"></avatar>
+              </div>
+            </template>
+            <v-list class="rounded-lg" style="background-color:#343a40;">
+              <v-list-item class="tile" v-for="(item,index) in items" :key="index">
+                <v-list-item-title style="color:#ced4da">{{ item.title }}</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
         </v-col>
         <v-col class="mt-8 mr-n16 ml-n16">
           <postButton></postButton>
@@ -36,10 +49,13 @@
   import Avatar from './TheAvatar.vue';
   import Notifications from './TheNotifications';
   import PostButton from '../postButton';
+  import UserMenu from '../perpage/TheUserMenu';
+
   import {
     simpleAxios,
     secureAxios
   } from '../../backend/axios.js';
+
   const LOGOUT_URL = '/api/v1/login';
 
   export default {
@@ -47,7 +63,37 @@
     components: {
       'avatar': Avatar,
       'notifications': Notifications,
-      'postButton': PostButton
+      'postButton': PostButton,
+      'userMenu': UserMenu
+    },
+    data() {
+      return {
+        items: [{
+            title: 'Profile'
+          },
+          {
+            title: 'Rooms'
+          },
+          {
+            title: 'Friends'
+          },
+          {
+            title: 'Create Post'
+          },
+          {
+            title: 'Edit Post'
+          },
+          {
+            title: 'Settings'
+          },
+          {
+            title: 'Help'
+          },
+          {
+            title: 'Sign out'
+          }
+        ],
+      }
     },
     methods: {
       setError(error, text) {
@@ -68,3 +114,13 @@
     }
   }
 </script>
+
+<style scoped>
+  .tile {
+    margin: 10px;
+    border-radius: 7px;
+  }
+  .tile:hover {
+    background: #2d00f7;
+  }
+</style>
