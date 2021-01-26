46bf449095f04071a279e44f2ef68703b3ec863f
satoru02 <sugiwaka02@gmail.com>
2021-01-26 16:05:10 +0900

    add date filter

--- app/helpers/filter_helper.rb
+++ app/helpers/filter_helper.rb
@@ -8,11 +8,37 @@ module FilterHelper
     if query === 'today'
       @filtered_posts = Post.where("created_at >= :start_date AND created_at <= :end_date", { start_date: Date.today, end_date: Date.today })
     elsif query === 'week'
-      @filtered_posts = Post.where("created_at >= :start_date AND created_at <= :end_date", { start_date: Date.today - 6, end_date: Date.today })
+      @filtered_posts = Post.where("created_at >= :start_date AND created_at <= :end_date", { start_date: Date.today - gap_days, end_date: Date.today })
     elsif query === 'month'
-      # @filtered_posts = Post.where("created_at >= :start_date AND created_at <= :end_date", { start_date: Date.today, end_date: Date.today })
+      @filtered_posts = Post.where("created_at >= :start_date AND created_at <= :end_date", { start_date: month_first_day, end_date: Date.today })
     end
+  end
+
+  def gap_days
+    day = Date.today.strftime("%A")
+    if day === "Monday"
+      0
+    elsif day === "Tuesday"
+      1
+    elsif day === "Wednesday"
+      2
+    elsif day === "Wednesday"
+      3
+    elsif day === "Thursday"
+      4
+    elsif day === "Friday"
+      5
+    elsif day === "Saturday"
+      6
+    elsif day === "Sunday"
+      7
+    end
+  end
+
+  def month_first_day
+    today = Date.today
+    first_day = Date.new(today.year, today.month, 1)
   end
 end
 
-    # @filtered_posts = Post.where("date = :specified_time", {specified_time: "1/21/2021"})
+# @filtered_posts = Post.where("date = :specified_time", {specified_time: "1/21/2021"})
