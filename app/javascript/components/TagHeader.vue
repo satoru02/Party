cc13d4bac03ea748b4f696f899b0e97c24776c8e
satoru02 <sugiwaka02@gmail.com>
2021-02-01 21:48:58 +0900

    add tag responsive

--- app/javascript/components/TagHeader.vue
+++ app/javascript/components/TagHeader.vue
@@ -1,20 +1,24 @@
 <template>
+<v-responsive class="overflow-y-auto flex-grow-1 flex-shrink-0" style="max-width: 80%;" height="400">
   <div class="tag">
     <v-container class="ml-16">
       <v-row no-gutters>
         <h2 class="mb-5 ml-8 mt-n3">ピックアップ</h2>
-        <v-col v-for="(tag, index) in tags" :key="index" class="mb-4 ml-8" cols="12" sm="16">
+        <v-col v-for="(tag, index) in tags" :key="index" class="ml-8" cols="12" sm="16">
+          <v-hover>
           <router-link :to="{name: 'Tag', params: {name: `${ tag.attributes.name }`}}">
-            <div class="tag_item" style="color:#d3d3d3; font-size: 15px;">#{{ tag.attributes.name }}</div>
+            <p class="tag_item" style="color:#d3d3d3; font-size: 15px;">#{{ tag.attributes.name }}</p>
           </router-link>
+          </v-hover>
         </v-col>
       </v-row>
     </v-container>
   </div>
+</v-responsive>
 </template>
 
 <script>
-  import { simpleAxios, secureAxios } from '../backend/axios.js'
+  import { simpleAxios } from '../backend/axios.js'
 
   const TAG_URL = `/api/v1/tags`
 
@@ -49,5 +53,9 @@
 
   .category_item {
     font-family: 'Roboto', sans-serif;
+  }
+
+  .tag_item:hover {
+    color: #000000;
   }
 </style>
