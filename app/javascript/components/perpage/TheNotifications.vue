92eb3782e5d43d62da9aea7334acb29adf2f6ec9
satoru02 <sugiwaka02@gmail.com>
2021-02-06 13:37:54 +0900

    fix notifications/entry and response

--- app/javascript/components/perpage/TheNotifications.vue
+++ app/javascript/components/perpage/TheNotifications.vue
@@ -5,22 +5,22 @@
         <v-menu open-on-hover offset-y left nudge-height=800 nudge-right="60" nudge-width=150>
           <template v-slot:activator="{ on, attrs }">
             <div v-bind="attrs" v-on="on">
-              <v-badge v-if="entryCount(notifications) > 0" color="red" :content="entryCount(notifications)" offset-y="10"
+              <v-badge v-if="entryNotifications.length > 0" color="red" :content="entryNotifications.length" offset-y="10"
                 offset-x="7">
                 <router-link to="/notifications">
                   <v-icon class="icon" size=24>mdi-bell-outline</v-icon>
                 </router-link>
               </v-badge>
               <router-link to="/notifications">
-                <v-icon v-if="entryCount(notifications) === 0" class="icon" size=24>mdi-bell-outline</v-icon>
+                <v-icon v-if="entryNotifications.length === 0" class="icon" size=24>mdi-bell-outline</v-icon>
               </router-link>
             </div>
           </template>
           <v-list class="overflow-y-auto rounded-s" max-height="650" style="background-color:#343a40;">
-            <v-list-item v-if="entryCount(notifications) === 0" class="tile">
+            <v-list-item v-if="entryNotifications.length === 0" class="tile">
               <v-list-item-title class="ml-10 mr-10" style="color:#ced4da">現在、新着のお知らせはありません。</v-list-item-title>
             </v-list-item>
-            <v-list-item class="tile" v-for="(notification, index) in entryAndResponseNotification(notifications)" :key="index">
+            <v-list-item class="tile" v-for="(notification, index) in entryNotifications" :key="index">
               <router-link :to="{ name: 'Notification', params: {id: `${notification.attributes.id }`}}">
                 <v-badge dot left inline color="#2176ff">
                   <v-list-item-title v-if="notification.attributes.classification === 'entry'" class="ml-10 mr-10"
@@ -40,14 +40,14 @@
         <v-menu>
           <template v-slot:activator="{on, attrs}">
             <div v-bind="attrs" v-on="on">
-              <v-badge color="red" offset-y="10" offset-x="7" v-if="messageCount(notifications) > 0"
-                :content="messageCount(notifications)">
+              <v-badge color="red" offset-y="10" offset-x="7" v-if="messageNotifications.length > 0"
+                :content="messageNotifications.length">
                 <router-link to="/rooms">
                   <v-icon class="icon mt-1" size=24>mdi-chat-outline</v-icon>
                 </router-link>
               </v-badge>
               <router-link to="/rooms">
-                <v-icon v-if="messageCount(notifications) === 0" class="icon" size=24>mdi-chat-outline</v-icon>
+                <v-icon v-if="messageNotifications.length === 0" class="icon" size=24>mdi-chat-outline</v-icon>
               </router-link>
             </div>
           </template>
@@ -58,9 +58,7 @@
 </template>
 
 <script>
-  import {
-    simpleAxios, secureAxios
-  } from '../../backend/axios.js'
+  import { secureAxios } from '../../backend/axios.js'
   import moment from 'moment';
   const NOTIFICATIONS_URL = '/api/v1/notifications'
 
@@ -68,7 +66,8 @@
     name: 'Notifications',
     data() {
       return {
-        notifications: [],
+        entryNotifications: [],
+        messageNotifications: []
       }
     },
     channels: {
@@ -80,11 +79,13 @@
           // 2.Entry_Responseがあった時（承認/拒否）
           // 3.Messageを受け取った時 -> chat_icon
           // 4.Notificationが読まれた時
-          // console.log(data)
-          if (this.$store.state.currentUser.data.attributes.id === data["target_user_id"] && data["condition"] !== "read") {
-            this.notifications.push(data)
+
+          if (this.$store.state.currentUser.data.attributes.id === data["target_user_id"] && data["condition"] !== "read" && (data.attributes.classification === "entry" || "entryResponse")) {
+            this.entryNotifications.push(data)
+          } else if(this.$store.state.currentUser.data.attributes.id === data["target_user_id"] && data.attributes.classification === "message"){
+            this.messageNotifications.push(data)
           } else if (this.$store.state.currentUser.data.attributes.id === data["target_user_id"] && data["condition"] === "read") {
-            this.notifications.pop()
+            this.entryNotifications.pop()
           }
         },
         disconnected() {}
@@ -109,31 +110,14 @@
           .catch(error => this.Failed(error))
       },
       Successful(response) {
-        this.notifications = response.data.data
+        this.entryNotifications = response.data.data.filter(data => (data.attributes.classification === "entry") || (data.attributes.classification === "entryResponse"))
+        this.messageNotifications = response.data.data.filter(data => data.attributes.classification === "message")
       },
       Failed(error) {
         this.error = (error.response && error.response.data && error.response.data.error) || ""
       },
       catchedTime(time) {
         return moment(time).format("YYYY/MM/DD hh:mm")
-      },
-      messageCount(notifications) {
-        if (notifications) {
-          var filterd_notifications = notifications.filter(notification => notification.attributes.classification === "message")
-          return filterd_notifications.length
-        }
-      },
-      entryCount(notifications) {
-        if (notifications) {
-          var filterd_notifications = notifications.filter(notification => (notification.attributes.classification === "entry") || (notification.attributes.classification === "entryResponse"))
-          return filterd_notifications.length
-        }
-      },
-      entryAndResponseNotification(notifications){
-        if (notifications){
-          var filterd_notifications = notifications.filter(notification => (notification.attributes.classification === "entry") || (notification.attributes.classification === "entryResponse"))
-          return filterd_notifications
-        }
       }
     }
   }
