5cadd2d795425760c9097c09d29a4bffe03dd0a2
satoru02 <sugiwaka02@gmail.com>
2021-02-11 00:43:55 +0900

    add relationship component

--- app/javascript/components/user/UserRelationships.vue
+++ app/javascript/components/user/UserRelationships.vue
@@ -2,7 +2,35 @@
   <v-responsive
     class="overflow-y-auto flex-grow-1 flex-shrink-0"
     style="max-width: 100%;"
-    height="1200">
+    height="700">
+    <v-row style="width: 732px;" class="ml-1">
+      <v-col cols=12 md=4 v-for="(user, index) in relationships" :key="index">
+        <router-link :to="{ name: 'MyEvents', params: {id: user.id} }">
+        <v-sheet
+          class="rounded-lg"
+          elevation=4
+          width=222
+          height=120
+          color="#212529"
+          style="border: 1px solid hsla(0,0%,100%,.1);">
+          <v-row>
+            <v-col cols=12 md=4 />
+            <v-col cols=12 md=4>
+              <base-avatar :size="50" />
+            </v-col>
+            <v-col cols=12 md=4 />
+          </v-row>
+          <v-row class="mt-n5">
+            <v-col cols=12 md=3 />
+            <v-col cols=12 md=5 class="ml-2">
+              <p>{{ user.attributes.username }}</p>
+            </v-col>
+            <v-col cols=12 md=3 />
+          </v-row>
+        </v-sheet>
+        </router-link>
+      </v-col>
+    </v-row>
   </v-responsive>
 </template>
 
