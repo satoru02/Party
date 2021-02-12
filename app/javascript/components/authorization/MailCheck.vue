b51adbd24053fef6801478c7c15963d27f606d4a
satoru02 <sugiwaka02@gmail.com>
2021-02-13 07:25:37 +0900

    add mailcheck responsive

--- app/javascript/components/authorization/MailCheck.vue
+++ app/javascript/components/authorization/MailCheck.vue
@@ -1,37 +1,47 @@
 <template>
   <div justify="center" align="center">
-    <v-sheet elevation=10 style="border: 1px solid hsla(0,0%,100%,.1);" color="#212529" height="200" class="rounded-lg mt-16"
-      width="650">
-      <v-row>
-        <v-col cols=12 md=2></v-col>
-        <v-col cols=12 md=8 class="mt-8">
-          <h1>Check your email</h1>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col cols=12 md=2></v-col>
-        <v-col cols=12 md=8 class="mt-4">
-          <h1>{{ this.email }}へメールが届いています！</h1>
-        </v-col>
-      </v-row>
-    </v-sheet>
-    <v-row class="mt-8">
-      <v-col cols=12 md=12></v-col>
+    <v-row>
+      <v-col cols=1 sm=1 md=1 lg=3 xl=3 />
+      <v-col cols=10 sm=10 md=10 lg=6 xl=6>
+        <v-sheet elevation=10 style="border: 1px solid hsla(0,0%,100%,.1); width:auto; height:auto; min-height:200;" color="#212529"
+          class="rounded-lg mt-16">
+          <v-row>
+            <v-col cols=2 sm=2 md=2 lg=2 xl=2 />
+            <v-col cols=8 sm=8 md=8 lg=8 xl=8 class="mt-8">
+              <h2>メールボックスを確認してみてください。</h2>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols=2 sm=2 md=2 lg=2 xl=2 />
+            <v-col cols=8 sm=8 md=8 lg=8 xl=8 class="mt-4">
+              <h3>{{ this.email }}へメールが届いています！</h3>
+            </v-col>
+          </v-row>
+        </v-sheet>
+      </v-col>
+      <v-col cols=1 sm=1 md=1 lg=3 xl=3 />
     </v-row>
   </div>
-
 </template>
 
 <script>
-export default {
-  name: 'MailCheck',
-  data(){
-    return {
-      email: ''
+  export default {
+    name: 'MailCheck',
+    data() {
+      return {
+        email: ''
+      }
+    },
+    created() {
+      this.checkSignedIn()
+      this.email = this.$route.params.email
+    },
+    methods: {
+      checkSignedIn() {
+        if (this.$store.state.signedIn) {
+          this.$router.replace('/')
+        }
+      }
     }
-  },
-  created(){
-    this.email = this.$route.params.email
   }
-}
 </script>
