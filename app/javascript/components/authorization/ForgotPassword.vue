acdda90dfa6268d7497d81834cfd6ac502014a69
satoru02 <sugiwaka02@gmail.com>
2021-02-13 07:39:48 +0900

    add forgot-password responsive

--- app/javascript/components/authorization/ForgotPassword.vue
+++ app/javascript/components/authorization/ForgotPassword.vue
@@ -1,57 +1,61 @@
 <template>
   <div justify="center" align="center">
-    <v-sheet elevation=10 style="border: 1px solid hsla(0,0%,100%,.1);" color="#212529" height="330"
-      class="rounded-lg mt-16" width="650">
-      <v-row>
-        <v-col cols=12 md=2></v-col>
-        <v-col cols=12 md=8 class="mt-8">
-          <h1>パスワードをリセットする</h1>
-        </v-col>
-      </v-row>
+    <v-row>
+      <v-col cols=1 sm=1 md=1 lg=3 xl=3 />
+      <v-col cols=10 sm=10 md=10 lg=6 xl=6>
+        <v-sheet elevation=10 style="border: 1px solid hsla(0,0%,100%,.1); width:auto; height:auto" color="#212529"
+          class="rounded-lg mt-16">
+          <v-row>
+            <v-col sm=2 md=2 lg=2 xl=2 class="hidden-xs-only" />
+            <v-col cols=12 sm=8 md=8 lg=8 xl=8 class="mt-8">
+              <h2>パスワードをリセットする</h2>
+            </v-col>
+          </v-row>
 
-      <v-row>
-        <v-col cols=12 md=1></v-col>
-        <v-col cols=12 md=2 class="ml-n4">
-          <h3 style="color:#edf2f4;">Eメール</h3>
-        </v-col>
-      </v-row>
-      <v-row class="mt-n4">
-        <v-col cols=12 md=1></v-col>
-        <v-col cols=12 md=10>
-          <v-text-field v-model="email" outlined filled dense></v-text-field>
-        </v-col>
-      </v-row>
+          <v-row>
+            <v-col cols=1 sm=1 md=1 lg=1 xl=1 />
+            <v-col cols=3 sm=2 md=2 lg=2 xl=2 class="ml-n4">
+              <h3 style="color:#edf2f4;">Eメール</h3>
+            </v-col>
+          </v-row>
 
-      <v-row>
-        <v-col cols=12 md=1></v-col>
-        <v-col cols=12 md=10>
-          <v-btn @click="resetPassword()" large depressed block color="#2d00f7">
-            <v-row>
-              <v-col cols=12 md=4></v-col>
-              <v-col cols=12 md=1>
-                <p class="mt-4 ml-1" style="font-size:1rem;">Eメールを送信する</p>
-              </v-col>
-            </v-row>
-          </v-btn>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col cols=12 md=1></v-col>
-        <v-col cols=12 md=2 class="mt-n4 ml-n5">
-          <router-link :to="{name: 'login'}">
-            <p style="font-size:0.7rem; color:#6c757d;">ログイン</p>
-          </router-link>
-        </v-col>
-        <v-col cols=12 md=2 class="mt-n4 ml-n10">
-          <router-link :to="{name: 'signup'}">
-            <p style="font-size:0.7rem; color:#6c757d;">アカウント登録</p>
-          </router-link>
-        </v-col>
-      </v-row>
+          <v-row class="mt-n4">
+            <v-col cols=1 sm=1 md=1 lg=1 xl=1 />
+            <v-col cols=10 sm=10 md=10 lg=10 xl=10>
+              <v-text-field v-model="email" outlined filled dense />
+            </v-col>
+          </v-row>
 
-    </v-sheet>
-    <v-row class="mt-8">
-      <v-col cols=12 md=12></v-col>
+          <v-row>
+            <v-col cols=1 m=1 md=1 lg=1 xl=1 />
+            <v-col cols=10 sm=10 md=10 lg=10 xl=10>
+              <v-btn @click="resetPassword()" large depressed block color="#2d00f7">
+                <v-row>
+                  <v-col cols=3 sm=4 md=4 lg=4 xl=4 />
+                  <v-col cols=1 sm=1 md=1 lg=1 xl=1>
+                    <p class="mt-4 ml-1" style="font-size:1rem;">Eメールを送信する</p>
+                  </v-col>
+                </v-row>
+              </v-btn>
+            </v-col>
+          </v-row>
+
+          <v-row>
+            <v-col cols=1 sm=1 md=1 lg=1 xl=1 />
+            <v-col cols=3 sm=2 md=2 lg=2 xl=2 class="mt-n4 ml-n5">
+              <router-link :to="{name: 'login'}">
+                <p style="font-size:0.7rem; color:#6c757d;">ログイン</p>
+              </router-link>
+            </v-col>
+            <v-col cols=3 sm=2 md=2 lg=2 xl=2 class="mt-n4 ml-n10">
+              <router-link :to="{name: 'signup'}">
+                <p style="font-size:0.7rem; color:#6c757d;">アカウント登録</p>
+              </router-link>
+            </v-col>
+          </v-row>
+        </v-sheet>
+      </v-col>
+      <v-col cols=1 sm=1 md=1 lg=3 xl=3 />
     </v-row>
     <v-snackbar top v-model="snackbar" color="pink">
       {{ text }}
@@ -66,7 +70,9 @@
 </template>
 
 <script>
-  import { secureAxios } from '../../backend/axios.js'
+  import {
+    secureAxios
+  } from '../../backend/axios.js'
   const PASSWORD_RESET_URL = '/api/v1/password_resets';
 
   export default {
@@ -107,3 +113,9 @@
     }
   }
 </script>
+
+<style scoped>
+  h3 {
+    font-size: 0.9rem;
+  }
+</style>
