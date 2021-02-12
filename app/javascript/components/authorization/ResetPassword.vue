06740f5f354e4ff69e38a9e622ec5968cf096c13
satoru02 <sugiwaka02@gmail.com>
2021-02-13 07:50:44 +0900

    add reset-password responsive

--- app/javascript/components/authorization/ResetPassword.vue
+++ app/javascript/components/authorization/ResetPassword.vue
@@ -1,76 +1,84 @@
 <template>
   <div justify="center" align="center">
-    <v-sheet elevation=10 style="border: 1px solid hsla(0,0%,100%,.1);" color="#212529" height="450"
-      class="rounded-lg mt-16" width="650">
-      <v-row>
-        <v-col cols=12 md=2></v-col>
-        <v-col cols=12 md=8 class="mt-8">
-          <h1>パスワードの再設定</h1>
-        </v-col>
-      </v-row>
+    <v-row>
+      <v-col cols=1 sm=1 md=3 lg=3 xl=3 />
+      <v-col cols=10 sm=10 md=6 lg=6 xl=6>
+        <v-sheet elevation=10 style="border: 1px solid hsla(0,0%,100%,.1); height:auto; width:auto;" color="#212529"
+          class="rounded-lg mt-16">
+          <v-row>
+            <v-col cols=2 sm=2 md=2 lg=2 xl=2 />
+            <v-col cols=8 sm=8 md=8 lg=8 xl=8 class="mt-8">
+              <h2>パスワードの再設定</h2>
+            </v-col>
+          </v-row>
 
-      <v-row>
-        <v-col cols=12 md=1></v-col>
-        <v-col cols=12 md=2 class="ml-n2">
-          <h3 style="color:#edf2f4;">パスワード</h3>
-        </v-col>
-      </v-row>
-      <v-row class="mt-n4">
-        <v-col cols=12 md=1></v-col>
-        <v-col cols=12 md=10>
-          <v-text-field v-model="password" outlined filled dense></v-text-field>
-        </v-col>
-      </v-row>
+          <v-row>
+            <v-col cols=1 sm=1 md=1 lg=1 xl=1 />
+            <v-col cols=3 sm=2 md=3 lg=2 xl=2 class="ml-n2">
+              <h3 style="color:#edf2f4;">パスワード</h3>
+            </v-col>
+          </v-row>
+          <v-row class="mt-n4">
+            <v-col cols=1 sm=1 md=1 lg=1 xl=1 />
+            <v-col cols=10 sm=10 md=10 lg=10 xl=10>
+              <v-text-field v-model="password" outlined filled dense />
+            </v-col>
+          </v-row>
 
-      <v-row>
-        <v-col cols=12 md=1></v-col>
-        <v-col cols=12 md=3 class="ml-n4">
-          <h3 style="color:#edf2f4;">パスワードの確認</h3>
-        </v-col>
-      </v-row>
-      <v-row class="mt-n4">
-        <v-col cols=12 md=1></v-col>
-        <v-col cols=12 md=10>
-          <v-text-field v-model="password_confirmation" outlined filled dense></v-text-field>
-        </v-col>
-      </v-row>
+          <v-row>
+            <v-col cols=1 sm=1 md=1 lg=1 xl=1 />
+            <v-col cols=4 sm=3 md=4 lg=3 xl=3 class="ml-n4">
+              <h3 style="color:#edf2f4;">パスワードの確認</h3>
+            </v-col>
+          </v-row>
 
-      <v-row>
-        <v-col cols=12 md=1></v-col>
-        <v-col cols=12 md=10>
-          <v-btn @click="resetPassword()" large depressed block color="#2d00f7">
-            <v-row>
-              <v-col cols=12 md=4></v-col>
-              <v-col cols=12 md=1>
-                <p class="mt-4 ml-10" style="font-size:1rem;">再設定</p>
-              </v-col>
-            </v-row>
-          </v-btn>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col cols=12 md=1></v-col>
-        <v-col cols=12 md=2 class="mt-n4 ml-n5">
-          <router-link :to="{name: 'login'}">
-            <p style="font-size:0.7rem; color:#6c757d;">ログイン</p>
-          </router-link>
-        </v-col>
-        <v-col cols=12 md=2 class="mt-n4 ml-n8">
-          <router-link :to="{name: 'signup'}">
-            <p style="font-size:0.7rem; color:#6c757d;">アカウント登録</p>
-          </router-link>
-        </v-col>
-      </v-row>
+          <v-row class="mt-n4">
+            <v-col cols=1 sm=1 md=1 lg=1 xl=1 />
+            <v-col cols=10 sm=10 md=10 lg=10 xl=10>
+              <v-text-field v-model="password_confirmation" outlined filled dense />
+            </v-col>
+          </v-row>
 
-    </v-sheet>
-    <v-row class="mt-8">
-      <v-col cols=12 md=12></v-col>
+          <v-row>
+            <v-col cols=1 sm=1 md=1 lg=1 xl=1 />
+            <v-col cols=10 sm=10 md=10 lg=10 xl=10>
+              <v-btn @click="resetPassword()" large depressed block color="#2d00f7">
+                <v-row>
+                  <v-col cols=4 sm=4 md=4 lg=4 xl=4 />
+                  <v-col cols=1 sm=1 md=1 lg=1 xl=1>
+                    <p class="mt-4 ml-10" style="font-size:1rem;">再設定</p>
+                  </v-col>
+                </v-row>
+              </v-btn>
+            </v-col>
+          </v-row>
+
+          <v-row>
+            <v-col cols=1 sm=1 md=1 lg=1 xl=1 />
+            <v-col cols=3 sm=2 md=2 lg=2 xl=2 class="mt-n4 ml-n5">
+              <router-link :to="{name: 'login'}">
+                <p style="font-size:0.7rem; color:#6c757d;">ログイン</p>
+              </router-link>
+            </v-col>
+            <v-col cols=3 sm=2 md=2 lg=2 xl=2 class="mt-n4 ml-n8">
+              <router-link :to="{name: 'signup'}">
+                <p style="font-size:0.7rem; color:#6c757d;">アカウント登録</p>
+              </router-link>
+            </v-col>
+          </v-row>
+
+        </v-sheet>
+      </v-col>
+      <v-col cols=1 sm=1 md=3 lg=3 xl=3 />
     </v-row>
   </div>
 </template>
 
 <script>
-  import { simpleAxios, secureAxios } from '../../backend/axios.js'
+  import {
+    simpleAxios,
+    secureAxios
+  } from '../../backend/axios.js'
   const PASSWORD_RESET_URL = '/api/v1/password_resets'
 
   export default {
@@ -116,3 +124,9 @@
     }
   }
 </script>
+
+<style scoped>
+  h3 {
+    font-size: 0.9rem;
+  }
+</style>
