e31be04953b60df49352f6e2b89c1967093025de
satoru02 <sugiwaka02@gmail.com>
2021-02-14 22:02:43 +0900

    fix relationship responsive

--- app/javascript/components/user/UserRelationships.vue
+++ app/javascript/components/user/UserRelationships.vue
@@ -1,33 +1,25 @@
 <template>
-  <v-responsive
-    class="overflow-y-auto flex-grow-1 flex-shrink-0"
-    style="max-width: 100%;"
-    height="700">
-    <v-row style="width: 732px;" class="ml-1">
-      <v-col cols=12 md=4 v-for="(user, index) in relationships" :key="index">
+  <v-responsive class="overflow-y-auto flex-grow-1 flex-shrink-0" style="max-width: 100%;" height="700">
+    <v-row class="ml-1">
+      <v-col cols=4 sm=4 md=4 lg=4 xl=4 v-for="(user, index) in relationships" :key="index">
         <router-link :to="{ name: 'MyEvents', params: {id: user.id} }">
-        <v-sheet
-          class="rounded-lg"
-          elevation=4
-          width=222
-          height=120
-          color="#212529"
-          style="border: 1px solid hsla(0,0%,100%,.1);">
-          <v-row>
-            <v-col cols=12 md=4 />
-            <v-col cols=12 md=4>
-              <base-avatar :size="50" />
-            </v-col>
-            <v-col cols=12 md=4 />
-          </v-row>
-          <v-row class="mt-n5">
-            <v-col cols=12 md=3 />
-            <v-col cols=12 md=5 class="ml-2">
-              <p>{{ user.attributes.username }}</p>
-            </v-col>
-            <v-col cols=12 md=3 />
-          </v-row>
-        </v-sheet>
+          <v-sheet class="rounded-lg" elevation=4 color="#212529"
+            style="border: 1px solid hsla(0,0%,100%,.1); width:auto; height:auto;">
+            <v-row no-gutters>
+              <v-col cols=1 sm=4 md=4 lg=4 xl=4 />
+              <v-col cols=5 sm=4 md=4 lg=4 xl=4 align="center">
+                <base-avatar :size="70" />
+                <v-row no-gutters>
+                  <v-col cols=4 sm=2 md=2 lg=2 xl=2 />
+                  <v-col cols=2 sm=8 md=8 lg=8 xl=8 align="start" class="ml-n2">
+                    <p>{{ user.attributes.username }}</p>
+                  </v-col>
+                  <v-col cols=2 sm=2 md=2 lg=2 xl=2 />
+                </v-row>
+              </v-col>
+              <v-col cols=1 sm=4 md=4 lg=4 xl=4 />
+            </v-row>
+          </v-sheet>
         </router-link>
       </v-col>
     </v-row>
@@ -35,18 +27,18 @@
 </template>
 
 <script>
-import BaseAvatar from '../../components/base/BaseAvatar';
+  import BaseAvatar from '../../components/base/BaseAvatar';
 
-export default {
-  name: "UserRelationships",
-  components: {
-    'base-avatar': BaseAvatar
-  },
-  props: {
-    relationships: {
-      type: Array,
-      required: true
+  export default {
+    name: "UserRelationships",
+    components: {
+      'base-avatar': BaseAvatar
+    },
+    props: {
+      relationships: {
+        type: Array,
+        required: true
+      }
     }
   }
-}
 </script>
