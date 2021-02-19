50e3aa40455044b7405011b5f738a25959ffdeae
satoru02 <sugiwaka02@gmail.com>
2021-02-19 16:31:31 +0900

    fix mail setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { :host => 'http://http://176.34.13.245/' }
+  config.action_mailer.default_url_options = { :host => 'http://176.34.13.245/' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
