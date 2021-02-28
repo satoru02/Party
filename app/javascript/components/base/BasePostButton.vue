4b3977c9ffe3d9e0afdfeb1be7aae1daa77eddb8
satoru02 <sugiwaka02@gmail.com>
2021-02-28 23:02:27 +0900

    add favicon

--- app/javascript/components/base/BasePostButton.vue
+++ app/javascript/components/base/BasePostButton.vue
@@ -1,6 +1,6 @@
 <template>
   <router-link to="/upload">
-    <v-btn style="background-color:#2d00f7; font-weight:bold; height:34px;" dark class="rounded-lg">告知</v-btn>
+    <v-btn style="background-color:#2d00f7; font-weight:bold; height:34px;" dark class="rounded-s">告知</v-btn>
   </router-link>
 </template>
 
